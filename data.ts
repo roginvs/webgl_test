55b913fa935751708f244d6d4f4aca36f068049e
Vasilii Rogin <vasilii@giosg.com>
2021-01-01 15:16:03 +0200

    Playing with vertexes

--- data.ts
+++ data.ts
@@ -37,27 +37,20 @@ export const oldVerttexes = new Float32Array([
   0.25,
 ]);
 
-export const vertexes = new Float32Array([
-  0,
-  0.5,
-  0,
-  -0.5,
-  -0.5,
-  0,
-  0.5,
-  -0.5,
-  0,
+export const vertexes = new Float32Array(
+  `
+0       0.5     0  
+-0.5   -0.5     0  
+0.5    -0.5     0 
 
-  0,
-  1,
-  0,
-  0.5,
-  1,
-  0,
-  0.5,
-  0.5,
-  0,
-]);
+0       1       0 
+0.5     1       0 
+0.5     0.5     0 
+`
+    .split(/ +/)
+    .filter((x) => x)
+    .map((x) => parseFloat(x))
+);
 
 export const transformMatrix = new Float32Array([
   1,
