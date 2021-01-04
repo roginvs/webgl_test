b28d92c1ffed372be91f19833eed9857c27304c4
Vasilii Rogin <vasilii@giosg.com>
2021-01-04 21:11:25 +0200

    Add second part of plane

--- data.ts
+++ data.ts
@@ -3,6 +3,10 @@ export const planeVertexes = new Float32Array(
 10  -10  0   1 1   0 0 1
 -10  10  0   0 0   0 0 1
 -10  -10  0   0 1   0 0 1
+
+10  10  0   1 0   0 0 1
+10  -10  0   1 1   0 0 1
+-10  10  0   0 0   0 0 1
 `
     .replace(/\r|\n/g, " ")
     .split(/ +/)
