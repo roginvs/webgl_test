fcfbb8f12f57de682711bf62140d1cca46003bf4
Vasilii Rogin <vasilii@giosg.com>
2021-01-06 15:46:15 +0200

    Prettify

--- loadImage.ts
+++ loadImage.ts
@@ -1,6 +1,11 @@
-export async function loadImage(
-  imgSrc = "https://spacerangers.gitlab.io/favicon.png"
-) {
+async function loadImageToCanvas<T>(
+  imgSrc: string,
+  callback: (
+    context: CanvasRenderingContext2D,
+    width: number,
+    height: number
+  ) => T
+): Promise<T> {
   // Block magic to bypass CORS issues
 
   const blob = await fetch(imgSrc).then((x) => x.blob());
@@ -19,47 +24,52 @@ export async function loadImage(
   canvas.width = image.width;
   canvas.height = image.height;
   context.drawImage(image, 0, 0);
-  const imageData = context.getImageData(0, 0, image.width, image.height);
+
+  const data = callback(context, image.width, image.height);
 
   URL.revokeObjectURL(url);
+  return data;
+}
+
+/**
+ * Loads image from src url
+ */
+export async function loadImage(
+  imgSrc = "https://spacerangers.gitlab.io/favicon.png"
+) {
+  const imageData = await loadImageToCanvas(
+    imgSrc,
+    (context, width, height) => {
+      return context.getImageData(0, 0, width, height);
+    }
+  );
+
   return imageData;
 }
+
+/**
+ * Loads cubebox texture.
+ * Output is 6 sub-images of source
+ */
 
 export async function loadCubebox(imgSrc = "cubebox.jpg") {
   // Block magic to bypass CORS issues
 
-  const blob = await fetch(imgSrc).then((x) => x.blob());
+  const data = await loadImageToCanvas(imgSrc, (context, width, height) => {
+    const size = width / 4;
+    if (height / 3 !== size) {
+      throw new Error("Wrong size");
+    }
 
-  const url = URL.createObjectURL(blob);
-
-  const image = new Image();
-  image.src = url;
-  await new Promise((r) => (image.onload = r));
-
-  const canvas = document.createElement("canvas");
-  const context = canvas.getContext("2d");
-  if (!context) {
-    throw new Error("Failed to get canvas");
-  }
-  canvas.width = image.width;
-  canvas.height = image.height;
-  context.drawImage(image, 0, 0);
-
-  const size = image.width / 4;
-  if (image.height / 3 !== size) {
-    throw new Error("Wrong size");
-  }
-
-  const data = [
-    [1, 0],
-    [0, 1],
-    [1, 1],
-    [2, 1],
-    [3, 1],
-    [1, 2],
-  ].map(([i, j]) => context.getImageData(i * size, j * size, size, size));
-
-  URL.revokeObjectURL(url);
+    return [
+      [1, 0],
+      [0, 1],
+      [1, 1],
+      [2, 1],
+      [3, 1],
+      [1, 2],
+    ].map(([i, j]) => context.getImageData(i * size, j * size, size, size));
+  });
 
   return data;
 }
