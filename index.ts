6de3fb85c4f7cdfe32375e06d5b04a3a2e185fd9
Vasilii Rogin <vasilii@giosg.com>
2021-01-06 21:20:48 +0200

    Add commented

--- index.ts
+++ index.ts
@@ -226,6 +226,8 @@ const load1 = loadImage("model.png").then((imgData) => {
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
+  // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+  // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   console.info("Model texture loaded");
 });
 
